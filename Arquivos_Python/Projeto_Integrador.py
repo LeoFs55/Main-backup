2d0e805e7c2f89eb1194ba50775217115180a416
unknown <Leonardo>
2024-05-02 16:28:27 -0300

    Modularizacao do Projeto Integrador

--- Arquivos_Python/Projeto_Integrador.py
+++ Arquivos_Python/Projeto_Integrador.py
@@ -1,10 +1,14 @@
+#Validacao do numero
+
 def num_validation(entrada):
     try:
         num = int(entrada)
         return True, num
     except ValueError:
-        return False,'O valor inserido não é um número.'
-    
+        return False,'O valor inserido não é um número.'        
+
+#Binario
+
 def bi_validation(numero):
 
     tupla_numero = tuple(numero)
@@ -16,13 +20,61 @@ def bi_validation(numero):
             continue
     return True, tupla_numero
 
-def calculo(entrada,quant):#Calculo de BINARIO PARA DECIMAL
+def calculo_bi_p_dec(entrada,quant):
     resultado = 0
     tupla_entrada = bi_validation(entrada)[1]
 
     for ind, i in enumerate(range((quant-1),-1,-1)):
         resultado += int(tupla_entrada[ind])*(2**(i))
-    return f'O número {entrada} é igual a {resultado}'
+    return resultado
+
+def calculo_dec_p_bi(num):
+    if num == '0':
+        return num
+    binario = ''
+    decimal  = int(num)
+    while decimal > 0:
+        resto = decimal % 2
+        binario = str(resto) + binario
+        decimal //= 2
+    return num
+
+def binario(num, base):
+    if base == 10:
+
+        while True:
+
+            entrada = num
+
+            if num_validation(entrada):
+                print(calculo_dec_p_bi(entrada))
+                break
+
+            else:
+                print(num_validation(entrada)[1])
+                continue
+
+    elif base == 2:
+        
+        while True:
+            entrada = num
+            if num_validation(entrada):
+                quant = len(entrada)
+
+                if bi_validation(entrada)[0]:
+                    print(calculo_bi_p_dec(entrada,quant))
+
+                else:
+                    print(bi_validation(entrada)[1])
+                    continue
+
+                break
+            else:
+                print(num_validation(entrada)[1])
+
+                continue
+
+#Octal
 
 def oct_validation(numero):
     tupla_numero = tuple(numero)
@@ -34,14 +86,57 @@ def oct_validation(numero):
             continue
     return True, tupla_numero
 
-def calculo_oct(entrada,quant):#Calculo de OCTAL PARA DECIMAL
+def calculo_dec_p_octal(num):
+    if num == '0':
+        return f'O número {num} é igual a 0 em OCTADECIMAL'
+    octal = ''
+    decimal  = int(num)
+    while decimal > 0:
+        resto = decimal % 8
+        octal = str(resto) + octal
+        decimal //= 8
+
+    return num
+
+def calculo_oct_p_dec(entrada,quant):#Calculo de OCTAL PARA DECIMAL
     resultado = 0
     tupla_entrada = oct_validation(entrada)[1]
 
     for ind, i in enumerate(range((quant-1),-1,-1)):
         resultado += int(tupla_entrada[ind])*(8**(i))
-    return f'O número {entrada} é igual a {resultado}'
-    
+    return resultado
+
+def octadecimal(num, base):
+    if base == 10:    
+        while True:
+            entrada = num
+            if num_validation(entrada):
+                print(calculo_dec_p_octal(entrada))
+                break
+            else:
+                print(num_validation(entrada)[1])
+                continue
+    elif base == 8:
+        while True:
+            entrada = num
+            if num_validation(entrada):
+                quant = len(entrada)
+
+                if oct_validation(entrada)[0]:
+                    print(calculo_oct_p_dec(entrada,quant))
+
+                else:
+                    print(oct_validation(entrada)[1])
+                    continue
+
+                break
+            else:
+                print(num_validation(entrada)[1])
+
+                continue
+
+#Hexadecimal
+
 def hex_validation(numero):
     lista_numero = list(numero)
     for ind ,i in enumerate(lista_numero):
@@ -65,36 +160,13 @@ def hex_validation(numero):
             continue
     return True, lista_numero
 
-def hex_calculo(entrada,quant):#Calculo de HEXA PARA DECIMAL
+def calculo_hex_p_dec(entrada,quant):#Calculo de HEXA PARA DECIMAL
     resultado = 0
     tupla_entrada = hex_validation(entrada)[1]
 
     for ind, i in enumerate(range((quant-1),-1,-1)):
         resultado += int(tupla_entrada[ind])*(16**(i))
-    return f'O número {entrada} é igual a {resultado}'
-
-def calculo_dec_p_bi(num):
-    if num == '0':
-        return f'O número {num} é igual a 0 em BINARIO'
-    binario = ''
-    decimal  = int(num)
-    while decimal > 0:
-        resto = decimal % 2
-        binario = str(resto) + binario
-        decimal //= 2
-    return f'O número {num} é igual a {binario} em BINARIO'
-
-def calculo_dec_p_octal(num):
-    if num == '0':
-        return f'O número {num} é igual a 0 em OCTADECIMAL'
-    octal = ''
-    decimal  = int(num)
-    while decimal > 0:
-        resto = decimal % 8
-        octal = str(resto) + octal
-        decimal //= 8
-
-    return f'O número {num} é igual a {octal} em OCTADECIMAL'
+    return resultado
 
 def calculo_dec_p_hexa(num):
     if num == '0':
@@ -119,7 +191,34 @@ def calculo_dec_p_hexa(num):
         else:
             hexa = str(resto) + hexa
 
-    return f'O número {num} é igual a {hexa} em HEXADECIMAL'
+    return num
+
+def hexadecimal(num, base):
+    if base == 10:    
+        while True:
+            entrada = num
+            if num_validation(entrada):
+                print(calculo_dec_p_hexa(entrada))
+                break
+            else:
+                print(num_validation(entrada)[1])
+                continue
+    if base == 16:
+        while True:
+            entrada = num
+            if True:
+                quant = len(entrada)
+
+                if hex_validation(entrada)[0]:
+                    print(calculo_hex_p_dec(entrada,quant))
+
+                else:
+                    print(hex_validation(entrada)[1])
+                    continue
+
+                break
+
+#Definindo qual o tipo de entrada
 
 def entrada():
     try:
@@ -149,87 +248,7 @@ def entrada():
                 continue
     except ValueError:
         print('Você não digitou um número')
-def dec_binario():
-    while True:
-        entrada = input('Digite um numero DECIMAL:')
-        if num_validation(entrada):
-            print(calculo_dec_p_bi(entrada))
-            break
-        else:
-            print(num_validation(entrada)[1])
-            continue
-def dec_octal():
-    while True:
-        entrada = input('Digite um numero DECIMAL:')
-        if num_validation(entrada):
-            print(calculo_dec_p_octal(entrada))
-            break
-        else:
-            print(num_validation(entrada)[1])
-            continue
-def dec_hexa():
-    while True:
-        entrada = input('Digite um numero DECIMAL:')
-        if num_validation(entrada):
-            print(calculo_dec_p_hexa(entrada))
-            break
-        else:
-            print(num_validation(entrada)[1])
-            continue
 
-def binario_dec():
-    while True:
-        entrada = input('Digite um numero BINARIO:')
-        if num_validation(entrada):
-            quant = len(entrada)
-
-            if bi_validation(entrada)[0]:
-                print(calculo(entrada,quant))
-
-            else:
-                print(bi_validation(entrada)[1])
-                continue
-
-            break
-        else:
-            print(num_validation(entrada)[1])
-
-            continue
-        
-def octal_dec():
-    while True:
-        entrada = input('Digite um numero em OCTAL:')
-        if num_validation(entrada):
-            quant = len(entrada)
-
-            if oct_validation(entrada)[0]:
-                print(calculo_oct(entrada,quant))
-
-            else:
-                print(oct_validation(entrada)[1])
-                continue
-
-            break
-        else:
-            print(num_validation(entrada)[1])
-
-            continue
-        
-def hexa_dec():
-    while True:
-        entrada = input('Digite um numero em HEXAL:')
-        if True:
-            quant = len(entrada)
-
-            if hex_validation(entrada)[0]:
-                print(hex_calculo(entrada,quant))
-
-            else:
-                print(hex_validation(entrada)[1])
-                continue
-
-            break
-        
 # print(f"""{20*'-'}
 # Calculado de BINARIO, OCTAL E HEXADECIMAL PARA DECIMAL
 # {20*'-'}
--- Arquivos_Python/modularizacao.py
+++ Arquivos_Python/modularizacao.py
@@ -1,3 +1,7 @@
 #entendendo Módulos
 #Nome:
 print(__name__)
+
+import Projeto_Integrador
+
+Projeto_Integrador.binario("1010",2)
